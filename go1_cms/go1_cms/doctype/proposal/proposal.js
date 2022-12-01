ac7fe1a1ce61707a1423137a8113d7ae79b781f1
Tridots <info@tridotstech.com>
2022-12-01 09:21:56 +0100

    send mail with message

--- go1_cms/go1_cms/doctype/proposal/proposal.js
+++ go1_cms/go1_cms/doctype/proposal/proposal.js
@@ -111,7 +111,7 @@ frappe.ui.form.on('Proposal', {
                                     'filters': { 'name': frm.doc.quotation },
                                     'fieldname': ['party_name', 'valid_till','company']
                                 },
-                                async: fasle,
+                                async: false,
                                 callback: function(data) {
                                     if (data.message) {
                                        
