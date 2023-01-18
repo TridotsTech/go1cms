daed2d3923a00d42abade2be6db260aff33f2f68
Tridots <info@tridotstech.com>
2023-01-18 16:07:34 +0100

    Alignment in PDF of Proposal

--- go1_cms/go1_cms/doctype/proposal/proposal.py
+++ go1_cms/go1_cms/doctype/proposal/proposal.py
@@ -1883,7 +1883,7 @@ def generate_pdf(page, name):
 	footer_template=""
 	page_template = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> <style>'
 	
-	page_template += "h3 {font-size: 12px;}h2 {font-size: 14px;}.page{padding: 60px 0;}.page{font-family: Poppins, 'Source Sans Pro' !important;}p{font-size:16px;}"
+	page_template += "h3 {font-size: 12px;}h2 {font-size: 14px;}.page{padding: 60px 0 !important;}.page{font-family: Poppins, 'Source Sans Pro' !important;}p{font-size:16px;}"
 	page_template += '.logo {height: 1.5rem;width: auto;margin-right: 1rem;}.logotype {display: flex;align-items: center;font-weight: 700;}'
 	page_template += 'hr{border-width: thin;}body{padding:-20px;}'
 	page_template += '.footer-columns {display: flex;justify-content: space-between;}'
